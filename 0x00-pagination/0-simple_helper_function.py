9bd5b6c9cc08b6365755160da9940d80d992b097
Fares <119789181+FaresYigeremu@users.noreply.github.com>
2024-10-27 23:53:38 +0300

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-""" a function named index_range that takes two integer 
-arguments page and page_size
+""" Python script that contains function named index_range
+that takes two integer arguments page and page_size.
 """
 
 from typing import Tuple
