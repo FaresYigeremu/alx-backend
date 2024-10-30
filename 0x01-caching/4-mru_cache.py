829652d30aefddb960a72a6554674c86498f2bef
Fares <faresyigeremu@gmail.com>
2024-10-30 13:33:47 +0000

    MRU Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,24 +1,40 @@
+#!/usr/bin/env python3
+""" a class MRUCache that inherits from BaseCaching """
 from base_caching import BaseCaching
-""" a class MRUCache that inherits from BaseCaching"""
+from collections import OrderedDict
 
 
 class MRUCache(BaseCaching):
+    """ MRU Class caching system.
+    """
     def __init__(self):
+        """ Initialise Class instance. """
         super().__init__()
+        self.cache_data = OrderedDict()
+        self.mru = ""
 
     def put(self, key, item):
+        """ Add an item in cache.
+        """
         if key is None or item is None:
             return
+
+        if key in self.cache_data:
+            del self.cache_data[key]
+
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            oldest_key = next(iter(self.cache_data))
-            del self.cache_data[oldest_key]
-            print(f"DISCARD: {oldest_key}")
+            if self.mru is not None:
+                del self.cache_data[self.mru]
+                print("DISCARD: {}".format(self.mru))
+
         self.cache_data[key] = item
+        self.mru = key
 
     def get(self, key):
+        """ Get an item by key.
+        """
         if key is None or key not in self.cache_data:
             return None
-        value = self.cache_data[key]
-        del self.cache_data[key]
-        self.cache_data[key] = value
-        return value
+
+        self.mru = key
+        return self.cache_data[key]
