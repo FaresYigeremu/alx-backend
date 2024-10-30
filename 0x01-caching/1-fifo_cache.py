f75214ae354e883d7670fc470639da3e5454436a
Fares <faresyigeremu@gmail.com>
2024-10-30 13:16:52 +0000

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" a class FIFOCache that inherits from BaseCaching """
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFO caching system. """
+
+    def __init__(self):
+        """ Initialise Class instance. """
+        super().__init__()
+        self.cache_keys = []
+
+    def put(self, key, item):
+        """ Add an item in cache. """
+        if key is None or item is None:
+            return
+        #  Check if cache is full.
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            first_key = self.cache_keys.pop(0)  # Discard the first item (FIFO)
+            del self.cache_data[first_key]
+            print(f'DISCARD: {first_key}')
+        self.cache_keys.append(key)
+        self.cache_data[key] = item  # Assign item to key in cache_data.
+
+    def get(self, key):
+        """ Get an item by key. """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
